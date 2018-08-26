044dcf07a03aff591e265de730998ed38b1ea401
Tyler Thompson <tyler.thompson@wwt.com>
2018-08-26 14:15:10 -0600

    Woops, had some leftover cruft in the objective-c code. And no, it wasn't the entire objective-c code - TT

--- CucumberSwift/XCTestCaseGenerator/XCTestCaseGenerator.m
+++ CucumberSwift/XCTestCaseGenerator/XCTestCaseGenerator.m
@@ -11,58 +11,6 @@
 #import "XCTest/XCTest.h"
 #import <objc/runtime.h>
 
-@interface JRInvocationGrabber : NSProxy {
-        id              target;
-        NSInvocation    *invocation;
-    }
-    @property (retain) id target;
-    @property (retain) NSInvocation *invocation;
-@end
-
-@implementation JRInvocationGrabber
-    @synthesize target, invocation;
-    
-    - (id)initWithTarget:(id)target_ {
-        self.target = target_;
-        return self;
-    }
-    
-    - (NSMethodSignature*)methodSignatureForSelector:(SEL)selector_ {
-        return [self.target methodSignatureForSelector:selector_];
-    }
-    
-    - (void)forwardInvocation:(NSInvocation*)invocation_ {
-        [invocation_ setTarget:self.target];
-        self.invocation = invocation_;
-    }
-    
-    #if !__has_feature(objc_arc)
-    - (void)dealloc {
-        self.target = nil;
-        self.invocation = nil;
-        [super dealloc];
-    }
-    #endif
-    
-@end
-
-@implementation NSInvocation (jr_block)
-    
-    + (id)jr_invocationWithTarget:(id)target_ block:(void (^)(id target))block_ {
-        JRInvocationGrabber *grabber = [[JRInvocationGrabber alloc] initWithTarget:target_];
-    #if !__has_feature(objc_arc)
-        [grabber autorelease];
-    #endif
-        block_(grabber);
-        return grabber.invocation;
-    }
-    
-@end
-void sayHello ( id self, SEL _cmd,... )
-{
-    NSLog (@"Hello");
-}
-
 @implementation XCTestCaseGenerator
     struct objc_method {
         SEL method_name;
@@ -72,7 +20,6 @@ void sayHello ( id self, SEL _cmd,... )
     
     + (XCTestCase *)initWithClassName:(NSString *)className :(XCTestCaseMethod *)method {
         Class testCase =  objc_allocateClassPair([XCTestCase class], [className UTF8String], 0);
-//        objc_registerClassPair(testCase);
         struct objc_method myMethod;
         myMethod.method_name = sel_registerName([method.name UTF8String]);
         myMethod.method_imp  = imp_implementationWithBlock(method.closure);
