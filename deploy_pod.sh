af020ae1f08ceb71f7e77bfbe39b7b8738bab07e
Tyler Thompson <tyler.thompson@wwt.com>
2018-10-07 20:43:42 -0600

    Looks like travis had some trouble pushing, let's help it - TT

--- deploy_pod.sh
+++ deploy_pod.sh
@@ -1,4 +1,7 @@
 # get the latest code
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git config --global push.default current
 git checkout master
 git reset --hard origin/master
 git clean -df
@@ -12,7 +15,7 @@ podspec-bump -w
 node edit-plist.js `podspec-bump --dump-version`
 git commit -am "[ci skip] publishing pod version: `podspec-bump --dump-version`" 
 git tag "`podspec-bump --dump-version`"
-git push origin HEAD -u $(podspec-bump --dump-version)
+git push https://${PERSONAL_ACCESS_TOKEN}@github.com/Tyler-Keith-Thompson/CucumberSwift.git HEAD -u $(podspec-bump --dump-version)
 git reset --hard
 git clean -df
 curl --data "{\"tag_name\": \"`podspec-bump --dump-version`\",\"target_commitish\": \"master\",\"name\": \"`podspec-bump --dump-version`\",\"body\": \"Release of version `podspec-bump --dump-version`\",\"draft\": false,\"prerelease\": false}" "https://api.github.com/repos/Tyler-Keith-Thompson/CucumberSwift/releases?access_token=$PERSONAL_ACCESS_TOKEN"
