cc675f6dc527543159faaf6671b67c31d25fba76
Tyler Thompson <tyler.thompson@wwt.com>
2019-03-16 15:02:37 -0600

    [ci skip] more of the AST tests from Cucumber - TT

--- CucumberSwiftTests/CucumberTests/CucumberTests.swift
+++ CucumberSwiftTests/CucumberTests/CucumberTests.swift
@@ -10,13 +10,47 @@ import Foundation
 import XCTest
 @testable import CucumberSwift
 
+extension Collection where Element == Token {
+    public var text:String {
+        return compactMap { (token) -> String? in
+            switch token {
+            case .newLine:
+                return nil
+            case .integer(let t):
+                return t
+            case .string(let t):
+                return t
+            case .docString(_):
+                return nil
+            case .match(let t):
+                return t
+            case .title(_):
+                return nil
+            case .description(_):
+                return nil
+            case .tag(_):
+                return nil
+            case .tableHeader(let t):
+                return "<\(t)>"
+            case .tableCell(_):
+                return nil
+            case .scope(_):
+                return nil
+            case .keyword(_):
+                return nil
+            }
+        }.joined()
+    }
+}
+
 class CucumberTests:XCTestCase {
+    
     typealias TestType = (feature:String, ast:String)
     
-    func testGoodExamples() {
+    private func getTests(atPath path: String) -> [String:TestType] {
         var tests:[String:TestType] = [:]
         let bundle = Bundle(for: CucumberTests.self)
-        let enumerator:FileManager.DirectoryEnumerator? = FileManager.default.enumerator(at: bundle.bundleURL.appendingPathComponent("testdata/good"), includingPropertiesForKeys: nil)
+        let enumerator:FileManager.DirectoryEnumerator? = FileManager.default.enumerator(at: bundle.bundleURL.appendingPathComponent(path), includingPropertiesForKeys: nil)
         while let url = enumerator?.nextObject() as? URL {
             if (url.absoluteString.hasSuffix(".feature")) {
                 if let string = try? String(contentsOf: url, encoding: .utf8) {
@@ -34,8 +68,50 @@ class CucumberTests:XCTestCase {
                 }
             }
         }
+        return tests
+    }
+    
+    private func testBackground(node:Node?, child:[String:Any], fileName:String) {
+        if let background = child["background"] as? [String:Any] {
+            guard let backgroundNode = node as? BackgroundNode else { XCTFail("No background node found");return }
+            let backgroundSteps:[Step] = backgroundNode.children.compactMap { $0 as? StepNode }.map { Step(with: $0) }
+            testSteps(scope: background, stepObjects: backgroundSteps, fileName: fileName)
+        }
+    }
+    
+    private func testSteps(scope:[String:Any], stepObjects:[Step], fileName:String) {
+        if let steps = scope["steps"] as? [[String:Any]] {
+            XCTAssertEqual(steps.count, stepObjects.count, "Step count is not the same")
+            for (i, step) in steps.enumerated() {
+                let stepObject = stepObjects[safe: i]
+                if let keyword = step["keyword"] as? String {
+                    if (keyword.trimmingCharacters(in: .whitespaces) == "*") { return }
+                    if (keyword.trimmingCharacters(in: .whitespaces) == "Gitt") {
+                        XCTAssert(stepObject?.keyword == .given)
+                        return
+                    }
+                    if (keyword.trimmingCharacters(in: .whitespaces) == "Når") {
+                        XCTAssert(stepObject?.keyword == .when)
+                        return
+                    }
+                    if (keyword.trimmingCharacters(in: .whitespaces) == "Så") {
+                        XCTAssert(stepObject?.keyword == .then)
+                        return
+                    }
+                    XCTAssertEqual(keyword.trimmingCharacters(in: .whitespaces), stepObject?.keyword.toString())
+                }
+                if let text = step["text"] as? String {
+                    XCTAssertEqual(text, stepObject?.tokens.text, "Text does not match in: \(fileName)")
+                }
+            }
+        }
+    }
+    
+    func testGoodExamples() {
+        let tests:[String:TestType] = getTests(atPath: "testdata/good")
+
         tests.forEach { (name, test) in
-            guard !name.contains("rule") else { return }
+            guard !name.contains("rule"), !name.contains("Tags/tags") else { return }
             let tokens = Lexer(test.feature, uri: "test.feature").lex()
             let ast = AST(tokens)
             if  let data = test.ast.data(using: .utf8),
@@ -51,40 +127,15 @@ class CucumberTests:XCTestCase {
                         XCTAssertEqual(featureNode.children.count, children.count, "Wrong number of children in \(name)")
                         for (childIndex, child) in children.enumerated() {
                             let node = featureNode.children[safe: childIndex]
-                            if let background = child["background"] as? [String:Any] {
-                                guard let backgroundNode = node as? BackgroundNode else { XCTFail("No background node found");return }
-                                let backgroundSteps:[Step] = backgroundNode.children.compactMap { $0 as? StepNode }.map { Step(with: $0) }
-                                if let steps = background["steps"] as? [[String:Any]] {
-                                    XCTAssertEqual(steps.count, backgroundSteps.count, "Background step count is not the same")
-                                    for (i, step) in steps.enumerated() {
-                                        let backgroundStep = backgroundSteps[safe: i]
-                                        if let keyword = step["keyword"] as? String {
-                                            if (keyword.trimmingCharacters(in: .whitespaces) == "*") { return }
-                                            XCTAssertEqual(keyword.trimmingCharacters(in: .whitespaces), backgroundStep?.keyword.toString())
-                                        }
-                                        if let text = step["text"] as? String {
-                                            XCTAssertEqual(text, backgroundStep?.match)
-                                        }
-                                    }
+                            testBackground(node: node, child: child, fileName: name)
+                            if let scenario = child["scenario"] as? [String:Any] {
+                                guard let scenarioType = scenario["keyword"] as? String else { return }
+                                if (scenarioType == "Scenario") {
+                                    guard let scenarioNode = node as? ScenarioNode else { XCTFail("No scenario node found in file: \(name)");return }
+                                    let scenarioSteps:[Step] = scenarioNode.children.compactMap { $0 as? StepNode }.map { Step(with: $0) }
+                                    testSteps(scope: scenario, stepObjects: scenarioSteps, fileName: name)
                                 }
                             }
-//                            if let scenario = child["scenario"] as? [String:Any] {
-//                                guard let scenarioNode = node as? ScenarioNode else { XCTFail("No scenario node found in file: \(name)");return }
-//                                let scenarioSteps:[Step] = scenarioNode.children.compactMap { $0 as? StepNode }.map { Step(with: $0) }
-//                                if let steps = scenario["steps"] as? [[String:Any]] {
-//                                    XCTAssertEqual(steps.count, scenarioSteps.count, "Scenario step count is not the same")
-//                                    for (i, step) in steps.enumerated() {
-//                                        let scenarioStep = scenarioSteps[safe: i]
-//                                        if let keyword = step["keyword"] as? String {
-//                                            if (keyword.trimmingCharacters(in: .whitespaces) == "*") { return }
-//                                            XCTAssertEqual(keyword.trimmingCharacters(in: .whitespaces), scenarioStep?.keyword.toString())
-//                                        }
-//                                        if let text = step["text"] as? String {
-//                                            XCTAssertEqual(text, scenarioStep?.match)
-//                                        }
-//                                    }
-//                                }
-//                            }
                         }
                     }
                 }
