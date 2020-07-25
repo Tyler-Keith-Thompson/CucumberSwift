9a7f6208f09109f8a225dabbd1a4fb0d275bb6bf
Tyler Thompson <tyler.keith.thompson@gmail.com>
2020-07-25 07:21:54 -0600

    [gherkin-dsl] - Got some shouldLoad tests around the DSL - TT

--- CucumberSwiftTests/DSLTests/DSLFeatureTests.swift
+++ CucumberSwiftTests/DSLTests/DSLFeatureTests.swift
@@ -16,6 +16,10 @@ class DSLFeatureTests: XCTestCase {
         Cucumber.shared.features.removeAll()
     }
     
+    override func tearDown() {
+        Cucumber.shouldRunWith = { _, _ in true }
+    }
+    
     func testFeatureTags() {
         let featureTitle = UUID().uuidString
         let feature =
@@ -27,7 +31,7 @@ class DSLFeatureTests: XCTestCase {
         
         XCTAssertEqual(feature.tags, ["tag1", "tag2"])
         XCTAssertEqual(feature.title, featureTitle)
-        XCTAssertEqual(feature.location.line, 22)
+        XCTAssertEqual(feature.location.line, 26)
         XCTAssertEqual(feature.location.column, 16)
     }
     
@@ -74,7 +78,85 @@ class DSLFeatureTests: XCTestCase {
         XCTAssert(called)
     }
     
-    #warning("Need to test shouldLoadWith both with a line and with tags")
+    func testDSLShouldLoadWithLine() {
+        var called = 0
+        let stepExecutes = {
+            called += 1
+        }
+        
+        Cucumber.shouldRunWith = { scenario, _ in
+            return shouldRun(scenario?.withLine(92))
+        }
+        
+        Feature("") {
+            Scenario("First", tags: ["t1"]) {
+                Given(a: stepExecutes())
+            }
+            Scenario("Second", tags: ["t2", "t3"]) {
+                Given(a: stepExecutes())
+            }
+            Scenario("Third", tags: ["t1", "t2"]) {
+                Given(a: stepExecutes())
+            }
+        }
+        
+        Cucumber.shared.executeFeatures()
+        
+        XCTAssertEqual(called, 1)
+    }
+    
+    func testDSLShouldLoadWithScenarioTitle() {
+        var called = 0
+        let stepExecutes = {
+            called += 1
+        }
+        
+        Cucumber.shouldRunWith = { scenario, _ in
+            return scenario?.title == "First"
+        }
+        
+        Feature("") {
+            Scenario("First") {
+                Given(a: stepExecutes())
+            }
+            Scenario("Second") {
+                Given(a: stepExecutes())
+            }
+        }
+        
+        Cucumber.shared.executeFeatures()
+        
+        XCTAssertEqual(called, 1)
+    }
+    
+    func testDSLShouldLoadWithTags() {
+        var called = 0
+        let stepExecutes = {
+            called += 1
+        }
+        
+        Cucumber.shouldRunWith = { _, tags in
+            return tags.contains("t1")
+        }
+        
+        Feature("") {
+            Scenario("First", tags: ["t1"]) {
+                Given(a: stepExecutes())
+            }
+            Scenario("Second", tags: ["t2", "t3"]) {
+                Given(a: stepExecutes())
+            }
+            Scenario("Third", tags: ["t1", "t2"]) {
+                Given(a: stepExecutes())
+            }
+        }
+        
+        Cucumber.shared.executeFeatures()
+        
+        XCTAssertEqual(called, 2)
+    }
+    
+    #warning("Is it possible to run a specific scenario outline example by line?")
     //NOTE: Used to be that you could run a specific example in a ScenarioOutline by line number
     //That might be damn near impossible with the DSL without parsing the Swift AST directly
 }
