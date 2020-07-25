9dcdc1d8eb324c17479cb22e745c6278e13e2299
Tyler Thompson <tyler.keith.thompson@gmail.com>
2020-07-25 17:48:45 -0600

    [rule-dsl] - Rules are now available in the DSL - TT

---
+++ CucumberSwift/DSL/RuleDSL.swift
@@ -0,0 +1,24 @@
+//
+//  RuleDSL.swift
+//  CucumberSwift
+//
+//  Created by Tyler Thompson on 7/25/20.
+//  Copyright © 2020 Tyler Thompson. All rights reserved.
+//
+
+import Foundation
+
+public struct Rule: ScenarioDSL {
+    public let scenarios: [Scenario]
+    public init(_ title:String, tags:[String] = [],
+                                        line:UInt = #line, column:UInt = #column,
+                                        @ScenarioBuilder _ content: () -> [ScenarioDSL]) {
+        scenarios = content().flatMap { $0.scenarios }
+    }
+    public init(_ title:String, tags:[String] = [],
+                                        line:UInt = #line, column:UInt = #column,
+                                        @ScenarioBuilder _ content: () -> ScenarioDSL) {
+        scenarios = content().scenarios
+    }
+
+}
