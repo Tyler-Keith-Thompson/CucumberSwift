a27236b3e7f1d229704d633eb6f1aeb2b47aa7ca
Tyler Thompson <tyler.keith.thompson@gmail.com>
2021-04-24 15:13:28 -0600

    [hook-execution-order] - Full hook tests in place - TT

--- Tests/CucumberSwiftTests/Runner/HookTests.swift
+++ Tests/CucumberSwiftTests/Runner/HookTests.swift
@@ -47,6 +47,33 @@ class HookTests: XCTestCase {
         XCTAssertEqual(executionOrder, ["BeforeFeature", "Given"])
     }
 
+    func testAfterFeatureHookTriggersAppropriately() {
+        Cucumber.shared.parseIntoFeatures("""
+        Feature: Some terse yet descriptive text of what is desired
+           Scenario: Some determinable business situation
+             Given some precondition
+             When some action is performed
+             Then some testable result is achieved
+
+            Scenario: Some other determinable business situation
+              Given some other precondition
+              When some action is performed
+              Then some testable result is achieved
+        """)
+
+        var executionOrder = [String]()
+        AfterFeature { feature in
+            executionOrder.append("AfterFeature")
+            XCTAssertEqual(feature.title, "Some terse yet descriptive text of what is desired")
+        }
+
+        Given("some precondition") { _, _ in executionOrder.append("Given") }
+
+        Cucumber.shared.executeFeatures()
+
+        XCTAssertEqual(executionOrder, ["Given", "AfterFeature"])
+    }
+
     func testBeforeScenarioHookTriggersAppropriately() {
         Cucumber.shared.parseIntoFeatures("""
         Feature: Some terse yet descriptive text of what is desired
@@ -77,11 +104,49 @@ class HookTests: XCTestCase {
         Cucumber.shared.executeFeatures()
 
         XCTAssertEqual(executionOrder, [
-                        "BeforeFeature",
-                        "BeforeScenario_Some determinable business situation",
-                        "Given some precondition",
-                        "BeforeScenario_Some other determinable business situation",
-                        "Given some other precondition"
+            "BeforeFeature",
+            "BeforeScenario_Some determinable business situation",
+            "Given some precondition",
+            "BeforeScenario_Some other determinable business situation",
+            "Given some other precondition"
+        ])
+    }
+
+    func testAfterScenarioHookTriggersAppropriately() {
+        Cucumber.shared.parseIntoFeatures("""
+        Feature: Some terse yet descriptive text of what is desired
+           Scenario: Some determinable business situation
+             Given some precondition
+             When some action is performed
+             Then some testable result is achieved
+
+            Scenario: Some other determinable business situation
+              Given some other precondition
+              When some action is performed
+              Then some testable result is achieved
+        """)
+
+        var executionOrder = [String]()
+        AfterFeature { feature in
+            executionOrder.append("AfterFeature")
+            XCTAssertEqual(feature.title, "Some terse yet descriptive text of what is desired")
+        }
+
+        AfterScenario { scenario in
+            executionOrder.append("AfterScenario_\(scenario.title)")
+        }
+
+        Given("some precondition") { _, _ in executionOrder.append("Given some precondition") }
+        Given("some other precondition") { _, _ in executionOrder.append("Given some other precondition") }
+
+        Cucumber.shared.executeFeatures()
+
+        XCTAssertEqual(executionOrder, [
+            "Given some precondition",
+            "AfterScenario_Some determinable business situation",
+            "Given some other precondition",
+            "AfterScenario_Some other determinable business situation",
+            "AfterFeature"
         ])
     }
 
@@ -119,17 +184,135 @@ class HookTests: XCTestCase {
         Cucumber.shared.executeFeatures()
 
         XCTAssertEqual(executionOrder, [
-                        "BeforeFeature",
-                        "BeforeScenario_Some determinable business situation",
-                        "BeforeStep_some precondition",
-                        "Given some precondition",
-                        "BeforeStep_some action is performed",
-                        "BeforeStep_some testable result is achieved",
-                        "BeforeScenario_Some other determinable business situation",
-                        "BeforeStep_some other precondition",
-                        "Given some other precondition",
-                        "BeforeStep_some action is performed",
-                        "BeforeStep_some testable result is achieved"
+            "BeforeFeature",
+            "BeforeScenario_Some determinable business situation",
+            "BeforeStep_some precondition",
+            "Given some precondition",
+            "BeforeStep_some action is performed",
+            "BeforeStep_some testable result is achieved",
+            "BeforeScenario_Some other determinable business situation",
+            "BeforeStep_some other precondition",
+            "Given some other precondition",
+            "BeforeStep_some action is performed",
+            "BeforeStep_some testable result is achieved"
+        ])
+    }
+
+    func testAfterStepHookTriggersAppropriately() {
+        Cucumber.shared.parseIntoFeatures("""
+        Feature: Some terse yet descriptive text of what is desired
+           Scenario: Some determinable business situation
+             Given some precondition
+             When some action is performed
+             Then some testable result is achieved
+
+            Scenario: Some other determinable business situation
+              Given some other precondition
+              When some action is performed
+              Then some testable result is achieved
+        """)
+
+        var executionOrder = [String]()
+        AfterFeature { feature in
+            executionOrder.append("AfterFeature")
+            XCTAssertEqual(feature.title, "Some terse yet descriptive text of what is desired")
+        }
+
+        AfterScenario { scenario in
+            executionOrder.append("AfterScenario_\(scenario.title)")
+        }
+
+        AfterStep { step in
+            executionOrder.append("AfterStep_\(step.match)")
+        }
+
+        Given("some precondition") { _, _ in executionOrder.append("Given some precondition") }
+        Given("some other precondition") { _, _ in executionOrder.append("Given some other precondition") }
+
+        Cucumber.shared.executeFeatures()
+
+        XCTAssertEqual(executionOrder, [
+            "Given some precondition",
+            "AfterStep_some precondition",
+            "AfterStep_some action is performed",
+            "AfterStep_some testable result is achieved",
+            "AfterScenario_Some determinable business situation",
+            "Given some other precondition",
+            "AfterStep_some other precondition",
+            "AfterStep_some action is performed",
+            "AfterStep_some testable result is achieved",
+            "AfterScenario_Some other determinable business situation",
+            "AfterFeature"
+        ])
+    }
+
+    func testBeforeAndAfterHooksTriggerAppropriately() {
+        Cucumber.shared.parseIntoFeatures("""
+        Feature: Some terse yet descriptive text of what is desired
+           Scenario: Some determinable business situation
+             Given some precondition
+             When some action is performed
+             Then some testable result is achieved
+
+            Scenario: Some other determinable business situation
+              Given some other precondition
+              When some action is performed
+              Then some testable result is achieved
+        """)
+
+        var executionOrder = [String]()
+        BeforeFeature { feature in
+            executionOrder.append("BeforeFeature")
+            XCTAssertEqual(feature.title, "Some terse yet descriptive text of what is desired")
+        }
+
+        AfterFeature { feature in
+            executionOrder.append("AfterFeature")
+            XCTAssertEqual(feature.title, "Some terse yet descriptive text of what is desired")
+        }
+
+        BeforeScenario { scenario in
+            executionOrder.append("BeforeScenario_\(scenario.title)")
+        }
+
+        AfterScenario { scenario in
+            executionOrder.append("AfterScenario_\(scenario.title)")
+        }
+
+        BeforeStep { step in
+            executionOrder.append("BeforeStep_\(step.match)")
+        }
+
+        AfterStep { step in
+            executionOrder.append("AfterStep_\(step.match)")
+        }
+
+        Given("some precondition") { _, _ in executionOrder.append("Given some precondition") }
+        Given("some other precondition") { _, _ in executionOrder.append("Given some other precondition") }
+
+        Cucumber.shared.executeFeatures()
+
+        XCTAssertEqual(executionOrder, [
+            "BeforeFeature",
+            "BeforeScenario_Some determinable business situation",
+            "BeforeStep_some precondition",
+            "Given some precondition",
+            "AfterStep_some precondition",
+            "BeforeStep_some action is performed",
+            "AfterStep_some action is performed",
+            "BeforeStep_some testable result is achieved",
+            "AfterStep_some testable result is achieved",
+            "AfterScenario_Some determinable business situation",
+            "BeforeScenario_Some other determinable business situation",
+            "BeforeStep_some other precondition",
+            "Given some other precondition",
+            "AfterStep_some other precondition",
+            "BeforeStep_some action is performed",
+            "AfterStep_some action is performed",
+            "BeforeStep_some testable result is achieved",
+            "AfterStep_some testable result is achieved",
+            "AfterScenario_Some other determinable business situation",
+            "AfterFeature"
         ])
     }
 }
